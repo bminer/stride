9308bdf7bdabb6c32e70011f73bd853dc4863c7c
Blake Miner <miner.blake@gmail.com>
2014-01-08 19:24:20 -0500

    Initial commit Stride is an enhanced version of step with global error handling ... and unfortunately very little API documentation. :P

---
+++ stride.js
@@ -0,0 +1,243 @@
+var EventEmitter = require("events").EventEmitter;
+
+/* Introducing Go: A [step](https://github.com/creationix/step/)-like
+	flow control library for Node.js that makes parallel execution,
+	serial execution, and error handling super extra painless.
+
+	Just pass so-called "step" functions to `Go`, and it will run them:
+
+	```
+	Go(
+		function() {
+			fs.readFile("foo.txt", this);
+		}, function() {
+			fs.readFile("bar.txt", this);
+		}
+	);
+	```
+
+	Each step will get `this`, which is the callback that you're
+	supposed to call once the step completes.
+
+	`Go(...)` returns a Node EventEmitter that emits the following:
+
+	"error" - Emitted each time an Error occurs
+	"done" - Emitted each time the final `this` callback is called
+		(usually only fired once)
+*/
+function Go() {
+	var steps = [];
+	var emitter = new EventEmitter();
+	var data = {};
+	//Build `steps` array and listen for events
+	for(var i = 0; i < arguments.length; i++) {
+		switch(arguments[i].name) {
+			case "_catch":
+				emitter.once("error", arguments[i]);
+				break;
+			case "_catchAll":
+				emitter.on("error", arguments[i]);
+				break;
+			case "_finally":
+				emitter.once("done", arguments[i]);
+				break;
+			default:
+				steps.push(arguments[i]);
+				break;
+		}
+	}
+	/* Return the `next()` function specific to a step. */
+	function getNext(index) {
+		var numCalls = 0,
+			maxCalls = Go.defaultMaxCalls,
+			errorRaised = false;
+		/* This function is passed as `this` to each step.
+			It's purpose is to invoke the next step, passing parameters,
+			as necessary.
+		*/
+		function next(err) {
+			if(maxCalls != null && ++numCalls > maxCalls) {
+				err = new Error("go: `this` was called more than " +
+					maxCalls + " times!");
+			}
+			//Process arguments
+			if(err) {
+				if(EventEmitter.listenerCount(emitter, "error") > 0)
+					emitter.emit("error", err);
+				else if(EventEmitter.listenerCount(emitter, "done") == 0)
+					process.nextTick(function() {
+						throw err; //Throw uncaught exception
+					})
+				if(!errorRaised)
+					emitDone(arguments);
+				errorRaised = true;
+			}
+			if(!errorRaised) {
+				//Call the next step
+				if(index + 1 < steps.length) {
+					//Remove first (error) argument
+					var args;
+					if(arguments.length > 0) {
+						args = new Array(arguments.length - 1);
+						for(var j = 0; j < args.length; j++)
+							args[j] = arguments[j + 1];
+					}
+					else
+						args = [];
+					//Call the next step, catching any errors
+					var thisStepsNext = getNext(index + 1);
+					try {
+						var result = steps[index + 1].apply(thisStepsNext, args);
+						//Call next step if this step ran synchronously
+						//and returned data
+						if(result !== undefined) {
+							thisStepsNext(null, result);
+						}
+					} catch(e) {
+						thisStepsNext(e);
+					}
+				}
+				else {
+					emitDone(arguments);
+				}
+			}
+		}
+		next.canBeCalled = function canBeCalled(numOfTimes) {
+			maxCalls = numOfTimes;
+			return next;
+		};
+		var parallelTotal = 0,
+			parallelDone = 0,
+			parallelArgs = [null];
+		/* Break this step into multiple parallel steps.
+			The first argument passed to each parallel step's callback must
+			be the Error.
+
+			The following `numDataArgs` arguments of each parallel step are
+			passed as `numDataArgs` arguments to the next step; thus, the
+			number of arguments passed to the next step is equal to the
+			number of parallel steps times `numDataArgs`.
+
+			If there is an error in any of the parallel steps, it will
+			be raised just like any other error.
+		*/
+		next.parallel = function(numDataArgs) {
+			numDataArgs = numDataArgs || 1;
+			//Create closure for argument index
+			return (function(index) {
+				return function parallel(err, data) {
+					//Save the error from the first failed parallel step
+					if(err && parallelArgs[0] == null) {
+						parallelArgs[0] = err;
+					}
+					//Save the next `numDataArgs` arguments
+					for(var i = 0; i < numDataArgs; i++) {
+						parallelArgs[index * numDataArgs + i + 1] =
+							arguments[i + 1];
+					}
+					//When all parallel steps are done...
+					if(++parallelDone >= parallelTotal) {
+						//Make sure that too many parallel steps didn't execute
+						if(parallelDone > parallelTotal)
+						{
+							parallelArgs[0] = new Error("go: Parallel step " +
+								"callback was called more than " +
+								parallelTotal + " times!");
+						}
+						//Call the next step
+						next.apply(null, parallelArgs);
+					}
+				}
+			})(parallelTotal++);
+		};
+		/* Create a new group of steps. Each group of steps will be
+			executed in parallel, just like a `next.parallel()` call.
+			The next step will receive an additional argument for each
+			`group()` call.  This additional argument will be an Array,
+			with each element containing the "second argument" of each
+			parallel step in the group.
+
+			For example, consider the following code in a given step:
+
+			```
+			var fileGroup = this.group();
+			fs.readFile("foo.txt", fileGroup() );
+			fs.readFile("bar.txt", fileGroup() );
+			```
+
+			The next step will then get one additional argument for the
+			`fileGroup`.  This additional argument will be an Array of
+			2 elements; the first will contain the contents of `foo.txt`
+			and the second element will contain the contents of `bar.txt`.
+		*/
+		next.group = function(numDataArgs) {
+			numDataArgs = numDataArgs || 1;
+			var groupCallback = next.parallel(1);
+			var groupTotal = 0,
+				groupDone = 0,
+				groupArgs = [],
+				groupError = null;
+			//Return a callback-generating function
+			return function parallelGroup() {
+				//Create closure for argument index
+				return (function(index) {
+					//Return a callback
+					return function parallelCallback(err, data) {
+						//Save the error from the first failed group step
+						if(err && groupError == null) {
+							groupError = err;
+						}
+						//Save the next `numDataArgs` arguments
+						for(var i = 0; i < numDataArgs; i++) {
+							groupArgs[index * numDataArgs + i] =
+								arguments[i + 1];
+						}
+						//When all parallel group steps are done...
+						if(++groupDone >= groupTotal) {
+							//Make sure that too many group steps didn't execute
+							if(groupDone > groupTotal && groupError == null)
+							{
+								groupError = new Error("go: Parallel group" +
+									"callback was called more than " +
+									groupTotal + " times!");
+							}
+							//Call the `groupCallback`
+							groupCallback.call(null, groupError, groupArgs);
+						}
+					};
+				})(groupTotal++);
+			};
+		}
+		/* Some data storage exposed to multiple steps */
+		next.data = function data(key, value) {
+			if(arguments.length == 0) {
+				return data;
+			} else if(arguments.length == 1) {
+				return data[key];
+			} else {
+				data[key] = value;
+			}
+		};
+		next.data.clean = function() {
+			data = {};
+		};
+		return next;
+	}
+	//Add function to the end to emit "done" event
+	function emitDone(rawArgs) {
+		if(EventEmitter.listenerCount(emitter, "done") > 0) {
+			var args = new Array(rawArgs.length + 1);
+			args[0] = "done";
+			for(var i = 0; i < rawArgs.length; i++)
+				args[i + 1] = rawArgs[i];
+			emitter.emit.apply(emitter, args);
+		}
+	}
+	//Run the first step
+	process.nextTick(function() {
+		getNext(-1)();
+	});
+	return emitter;
+}
+Go.defaultMaxCalls = 1;
+module.exports = Go;
---
+++ test/stride.js
@@ -0,0 +1,396 @@
+var stride = require("../");
+
+//nodeunit tests}
+exports.callsCallbacks = function(test) {
+	test.expect(7);
+	var x = 1;
+	stride(
+		function() {
+			test.equal(x, 1);
+			x = 2;
+			this(null);
+			test.equal(x, 5);
+			test.done();
+		},
+		function() {
+			test.equal(x, 2);
+			x = 3;
+			this(/* we allow `null` to be missing cuz we are cool */);
+			test.equal(x, 4);
+			x = 5;
+			test.equal(x, 5);
+		},
+		function() {
+			test.equal(x, 3);
+			x = 4;
+			this(/* we allow `null` to be missing cuz we are cool */);
+			test.equal(x, 4);
+		}
+	);
+}
+
+exports.catchesErrors = function(test) {
+	test.expect(7);
+	var x = 1;
+	stride(
+		function() {
+			test.equal(x, 1);
+			x = 2;
+			this();
+			test.equal(x, "err");
+			test.done();
+		},
+		function() {
+			test.equal(x, 2);
+			x = 3;
+			throw new Error("This is an error.");
+		},
+		function() {
+			test.ok(false); //should not get here
+			test.equal(x, 3);
+			x = 4;
+			this();
+			test.equal(x, 4);
+		}
+	).on("error", function(err) {
+		test.equal(x, 3);
+		x = "err";
+		test.equal(err.toString(), "Error: This is an error.");
+	}).on("done", function(err) {
+		test.equal(x, "err");
+		test.equal(err.message, "This is an error.");
+	});
+}
+
+exports.handlesMultipleCallbackProblem = function(test) {
+	test.expect(16);
+	var x = 1;
+	stride(
+		function() {
+			test.equal(x, 1);
+			x = 2;
+			this(null, "arg1", "arg2");
+			test.equal(x, 5);
+			this(null, "arg3", "arg4");
+			test.equal(x, 7);
+			this(null, "arg5", "arg6");
+			test.equal(x, 7);
+			test.done();
+		},
+		function(arg1, arg2) {
+			test.equal(x, 2);
+			x = 3;
+			test.equal(arg1, "arg1");
+			test.equal(arg2, "arg2");
+			test.equal(2, arguments.length);
+			this();
+		},
+		function() {
+			test.equal(x, 3);
+			x = 4;
+			this(null, "foo", "bar");
+		}
+	).once("error", function(err) {
+		test.equal(1, arguments.length);
+		test.equal(x, 5);
+		test.ok(err.message.indexOf("called more than") >= 0);
+		x++;
+	}).on("done", function(err, foo, bar) {
+		if(x == 4) {
+			test.equal(3, arguments.length);
+			test.equal(err, null);
+			test.equal(foo, "foo");
+			test.equal(bar, "bar");
+			x = 5;
+		}
+		else
+			x++;
+	});
+}
+
+exports.handlesMultipleCallbacks = function(test) {
+	test.expect(25);
+	var x = 1;
+	stride(
+		function() {
+			this.canBeCalled(3);
+			test.equal(x, 1);
+			x++;
+			this(null, "arg1", "arg2");
+			test.equal(x, 5);
+			this(null, "arg3", "arg4");
+			test.equal(x, 8);
+			this(null, "arg5", "arg6");
+			test.equal(x, 11);
+			test.done();
+		},
+		function(arg1, arg2) {
+			if(x == 2) {
+				test.equal(arg1, "arg1");
+				test.equal(arg2, "arg2");
+			} else if(x == 5) {
+				test.equal(arg1, "arg3");
+				test.equal(arg2, "arg4");
+			} else {
+				test.equal(arg1, "arg5");
+				test.equal(arg2, "arg6");
+			}
+			x++;
+			test.equal(2, arguments.length);
+			this();
+		},
+		function() {
+			x++;
+			this(null, "foo", "bar");
+		}
+	).once("error", function(err) {
+		test.ok(false);
+		x = "err";
+	}).on("done", function(err, foo, bar) {
+		test.equal(3, arguments.length);
+		test.equal(err, null);
+		test.equal(foo, "foo");
+		test.equal(bar, "bar");
+		x++;
+	});
+}
+
+exports.notCompatibleWithStepSilliness = function(test) {
+	test.expect(6);
+	var x = 1;
+	stride(
+		function() {
+			this.canBeCalled(2);
+			setTimeout(this.bind(null, null, "timer1"), 100);
+			setTimeout(this.bind(null, null, "timer2"), 200);
+		},
+		function(timer) {
+			test.equals(timer, "timer" + x);
+			x++;
+			setTimeout(this.bind(null, null, "timer3"), 300);
+		},
+		function(timer) {
+			test.equals(timer, "timer3");
+			x++;
+			this();
+		}
+	).on("done", function(err) {
+		test.equals(arguments.length, 0);
+		if(x == 5)
+			test.done();
+	});
+}
+
+exports.supportsParallelCallbacks = function(test) {
+	test.expect(13);
+	var x = 1;
+	stride(
+		function() {
+			test.equal(x, 1);
+			setTimeout(this.parallel().bind(null, null, "foo"), 100);
+			setTimeout(this.parallel(), 300);
+			setTimeout(this.parallel().bind(null, null, "foo", "bar"), 200);
+			x = 2;
+			var cb = this.parallel();
+			setTimeout(function() {
+				test.equal(x, 2);
+				x = 3;
+				cb(null, "idiot");
+			}, 400);
+		},
+		function(arg1, arg2, arg3, arg4) {
+			test.equal(x, 3);
+			x = 4;
+			test.equal(arg1, "foo");
+			test.equal(arg2, undefined);
+			test.equal(arg3, "foo");
+			test.equal(arg4, "idiot");
+			test.equal(4, arguments.length);
+			this(null, arg1);
+			test.equals(x, "done");
+			test.done();
+		}
+	).on("error", function(err) {
+		x = "err";
+		test.ok(false); //should not get here
+	}).on("done", function(err, arg1) {
+		test.equals(x, 4);
+		x = "done";
+		test.equals(2, arguments.length);
+		test.equals(err, null);
+		test.equals(arg1, "foo");
+	});
+};
+
+exports.supportsParallelCallbacksWithError = function(test) {
+	test.expect(11);
+	var x = 1;
+	stride(
+		function() {
+			test.equal(x, 1);
+			setTimeout(this.parallel().bind(null, null, "foo"), 100);
+			setTimeout(this.parallel().bind(null, new Error("Oops!") ), 300);
+			setTimeout(this.parallel().bind(null, null, "foo", "bar"), 200);
+			x = 2;
+			var cb = this.parallel();
+			setTimeout(function() {
+				test.equal(x, 2);
+				x = 3;
+				cb(null, "idiot");
+			}, 400);
+		},
+		function(arg1, arg2, arg3, arg4) {
+			test.ok(false); //should not get here
+			this();
+		}
+	).on("error", function(err) {
+		test.equals(x, 3);
+		test.equals(arguments.length, 1);
+		x = "err";
+		test.equals(err.message, "Oops!");
+	}).on("done", function(err, arg1, arg2, arg3, arg4) {
+		test.equals(x, "err");
+		x = "done";
+		test.equals(arguments.length, 5);
+		test.equal(arg1, "foo");
+		test.equal(arg2, undefined);
+		test.equal(arg3, "foo");
+		test.equal(arg4, "idiot");
+		test.done();
+	});
+};
+
+exports.handlesErrorWithoutErrorListener = function(test) {
+	test.expect(2);
+	stride(
+		function() {
+			this(new Error("Oops!") );
+		}
+	).on("done", function(err) {
+		test.equals(arguments.length, 1);
+		test.equals(err.message, "Oops!")
+		test.done();
+	});
+};
+
+exports.handlesErrorWithoutDoneListener = function(test) {
+	test.expect(2);
+	stride(
+		function() {
+			this(new Error("Oops!") );
+		}
+	).on("error", function(err) {
+		test.equals(arguments.length, 1);
+		test.equals(err.message, "Oops!")
+		test.done();
+	});
+};
+
+exports.canThrowUncaughtException = function(test) {
+	test.expect(1);
+	process.once("uncaughtException", function(err) {
+		test.equal(err.message, "Oops!");
+		test.done();
+	});
+	stride(
+		function() {
+			this(new Error("Oops!") );
+		}
+	);
+};
+
+exports.supportsGroups = function(test) {
+	test.expect(5);
+	var x = 1;
+	stride(
+		function() {
+			var group = this.group();
+			setTimeout(group().bind(null, null, "foo", "foo2"), 200);
+			setTimeout(group().bind(null, null, "bar", "bar2"), 100);
+		},
+		function(data) {
+			test.equals(arguments.length, 1);
+			test.equals(data.length, 2);
+			test.equals(data[0], "foo");
+			test.equals(data[1], "bar");
+			this();
+		}
+	).on("done", function(err) {
+		test.equals(0, arguments.length);
+		test.done();
+	});
+}
+
+exports.supportsGroupsWithMultipleArguments = function(test) {
+	test.expect(8);
+	var x = 1;
+	stride(
+		function() {
+			var group = this.group(2);
+			setTimeout(group().bind(null, null, "foo", "foo2"), 300);
+			setTimeout(group().bind(null, null, "bar", "bar2"), 100);
+			setTimeout(this.parallel().bind(null, null, "parallel", "parallel2"), 200);
+		},
+		function(data, parallel) {
+			test.equals(arguments.length, 2);
+			test.equals(data.length, 4);
+			test.equals(data[0], "foo");
+			test.equals(data[1], "foo2");
+			test.equals(data[2], "bar");
+			test.equals(data[3], "bar2");
+			test.equals(parallel, "parallel");
+			this();
+		}
+	).on("done", function(err) {
+		test.equals(0, arguments.length);
+		test.done();
+	});
+}
+
+exports.worksSynchronously = function(test) {
+	test.expect(16);
+	var x = 1;
+	stride(
+		function() {
+			test.equal(x, 1);
+			x = 2;
+			return "foo";
+			test.ok(false);
+		},
+		function(data) {
+			test.equal(x, 2);
+			test.equal(data, "foo");
+			test.equal(arguments.length, 1);
+			var p1 = this.parallel(),
+				p2 = this.parallel();
+			p1(null, "synchronous parallel");
+			p2(null, "synchronous parallel 2");
+			test.equal(x, 3);
+			x = 4;
+			setTimeout(function() {
+				test.equal(x, 4);
+				x = 5;
+			}, 100);
+		},
+		function(p1, p2) {
+			test.equal(arguments.length, 2);
+			test.equal(x, 2);
+			test.equal(p1, "synchronous parallel");
+			test.equal(p2, "synchronous parallel 2");
+			x = 3;
+			setTimeout(this, 200);
+		},
+		function() {
+			test.equal(arguments.length, 0);
+			test.equal(x, 5);
+			x = 6;
+			return null;
+		}
+	).on("done", function(err, data) {
+		test.equals(x, 6);
+		test.equals(arguments.length, 2);
+		test.equals(err, null);
+		test.equals(data, null);
+		test.done();
+	});
+}
